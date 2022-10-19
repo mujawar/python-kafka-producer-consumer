88bdb66147694b6732e958048077577cac9644e2
Arif <mahammad.arif1@effem.com>
2022-10-19 10:42:00 +0530

    code changes

--- kafka-python-code/kafka_consumer.py
+++ kafka-python-code/kafka_consumer.py
@@ -1,13 +1,15 @@
-import json 
 from kafka import KafkaConsumer
+from json import loads
 
+consumer = KafkaConsumer(
+    'numbertest', # topic
+     group_id=None,
+     bootstrap_servers=['localhost:9092'], # bootstrap server
+     api_version=(0,11,3),
+     auto_offset_reset='earliest',
+     enable_auto_commit=True,
+     value_deserializer=lambda x: loads(x.decode('utf-8')))
 
-if __name__ == '__main__':
-    # Kafka Consumer 
-    consumer = KafkaConsumer(
-        'messages',
-        bootstrap_servers='localhost:9092',
-        auto_offset_reset='earliest'
-    )
-    for message in consumer:
-        print(json.loads(message.value))
+print('Receiving message...')
+for message in consumer:
+    print('Message: ' + str(message.value))
