19ddf05ee3351f1d11fb146643eeb045774e0d6b
Arif <mahammad.arif1@effem.com>
2022-11-07 14:04:50 +0530

    producer code added

--- kafka-python-code/kafka_consumer.py
+++ kafka-python-code/kafka_consumer.py
@@ -1,15 +1,13 @@
+import json 
 from kafka import KafkaConsumer
-from json import loads
 
-consumer = KafkaConsumer(
-    'numbertest', # topic
-     group_id=None,
-     bootstrap_servers=['localhost:9092'], # bootstrap server
-     api_version=(0,11,3),
-     auto_offset_reset='earliest',
-     enable_auto_commit=True,
-     value_deserializer=lambda x: loads(x.decode('utf-8')))
 
-print('Receiving message...')
-for message in consumer:
-    print('Message: ' + str(message.value))
+if __name__ == '__main__':
+    # Kafka Consumer 
+    consumer = KafkaConsumer(
+        'numbertest1',
+        bootstrap_servers='localhost:9092',
+        auto_offset_reset='earliest'
+    )
+    for message in consumer:
+        print(json.loads(message.value))
--- kafka-python-code/kafka_producer.py
+++ kafka-python-code/kafka_producer.py
@@ -1,15 +1,21 @@
 from time import sleep
 from json import dumps
 from kafka import KafkaProducer
+import json
+import pandas as pd
+
+df = pd.read_csv('./2016_US_election_tweets_100k.csv')
+josndata =df.to_dict('records')
+# print(josndata)
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          api_version=(0,11,3),
                          value_serializer=lambda x:dumps(x).encode('utf-8'))
 print('Processing...')
-for num in range(1000):
-    data={'number': num}
+for row_data in josndata:
+    # print("data----------->",row_data)
     # send message
-    producer.send('numbertest', data)
-    print('send data: ' + str(data))
+    producer.send('numbertest1', row_data)
+    print('send data: ' + str(row_data))
     # sleep 2s
     sleep(2)
